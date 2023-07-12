7f70011a6763a5ab4bd91b1c0555cfe81350d00e
nick2918 <35611616+nick2918@users.noreply.github.com>
2023-07-12 23:48:32 +0000

    semaforo terminado

--- src/js/component/semaforo.jsx
+++ src/js/component/semaforo.jsx
@@ -6,13 +6,16 @@ const Home = () => {
 	const [color, setColor] = useState("secondary")
 	return (
 		
-		<div className="text-center d-grid gap-2 col-4 mx-auto justify-content">
-			<button onClick={()=> setColor("danger")}
-		 	className={"bg-" + (color==="danger"? "danger":"secondary")} style={{width: "100px", height:"100px", borderRadius:"100%", marginTop:"10px", marginLeft:"250px"}}></button>
-			<button onClick={()=> setColor("warning")}
-		 	className={"bg-" + (color==="warning"? "warning":"secondary")} style={{width: "100px", height:"100px", borderRadius:"100%", marginTop:"10px", marginLeft:"250px"}}></button>
-			<button onClick={()=> setColor("success")}
-		 	className={"bg-" + (color==="success"? "success":"secondary")} style={{width: "100px", height:"100px", borderRadius:"100%", marginTop:"10px", marginLeft:"250px"}}></button>
+		<div className="text-center d-grid col-3 mx-auto justify-content">
+			<div className="text-center d-grid col-3 mx-auto justify-content" style={{backgroundColor: "#181818", width:"50px", height:"70px"}}></div>
+			<div className="text-center d-grid mx-auto justify-content" style={{backgroundColor: "#181818", width:"200px", borderRadius:"20px", marginTop:"0px"}}> 
+				<div onClick={()=> setColor("danger")}
+				className={"bg-" + (color==="danger"? "danger":"secondary")} style={{width: "100px", height:"100px", borderRadius:"100%", marginTop:"10px", marginLeft:"50px"}}></div>
+				<div onClick={()=> setColor("warning")}
+				className={"bg-" + (color==="warning"? "warning":"secondary")} style={{width: "100px", height:"100px", borderRadius:"100%", marginTop:"10px", marginLeft:"50px"}}></div>
+				<div onClick={()=> setColor("success")}
+				className={"bg-" + (color==="success"? "success":"secondary")} style={{width: "100px", height:"100px", borderRadius:"100%", marginTop:"10px", marginLeft:"50px", marginBottom:"10px"}}></div>
+			</div>
 			<button onClick={()=> setColor("secondary")} type="button" class="btn btn-primary mt-2">Reset</button>
 		</div>
 		
